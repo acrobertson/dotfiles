6db925b617582731a12473332c00151080439c9b
Alec Robertson <alec.robertson08@gmail.com>
2022-10-22 17:13:58 -0400

    feat(nvim): show dashboard when all tabs close

--- home/dot_config/astronvim/lua/user/polish.lua
+++ home/dot_config/astronvim/lua/user/polish.lua
@@ -12,6 +12,26 @@ return function()
     command = "source <afile> | PackerSync",
   })
 
+  -- Show Alpha dashboard when no more buffers
+  local function alpha_on_bye(cmd)
+    local bufs = vim.fn.getbufinfo { buflisted = true }
+    vim.cmd(cmd)
+    if require("core.utils").is_available "alpha-nvim" and not bufs[2] then
+      require("alpha").start(true)
+    end
+  end
+
+  vim.keymap.del("n", "<leader>c")
+  if require("core.utils").is_available "bufdelete.nvim" then
+    vim.keymap.set("n", "<leader>c", function()
+      alpha_on_bye "Bdelete!"
+    end, { desc = "Close buffer" })
+  else
+    vim.keymap.set("n", "<leader>c", function()
+      alpha_on_bye "bdelete!"
+    end, { desc = "Close buffer" })
+  end
+
   -- Set up custom filetypes
   -- vim.filetype.add {
   --   extension = {
