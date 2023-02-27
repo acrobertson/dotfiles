0aea0e740f6c9d3b1f5f0b1759ac067b24fac898
Alec Robertson <alec.robertson08@gmail.com>
2023-02-27 11:01:02 -0500

    feat (nvim): add listchars toggle

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,26 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+local function toggle_list()
+  if vim.opt.list:get() then
+    vim.cmd("setlocal nolist")
+    vim.b.miniindentscope_disable = true
+  else
+    vim.cmd("setlocal list")
+    vim.b.miniindentscope_disable = false
+  end
+end
+
+map("n", "<leader>uL", toggle_list, { desc = "Toggle listchars" })
--- home/dot_config/nvim/lua/plugins/ui.lua
+++ home/dot_config/nvim/lua/plugins/ui.lua
@@ -1,8 +1,21 @@
 return {
+  -- set bg color for transparent colorscheme
   {
     "rcarriga/nvim-notify",
     opts = {
       background_colour = "#1a1b26",
     },
+  },
+
+  -- disable indent plugins if nolist is set
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    opts = {
+      disable_with_nolist = true,
+    },
+  },
+  {
+    "echasnovski/mini.indentscope",
+    cond = vim.opt.list:get(),
   },
 }
