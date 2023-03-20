2a1cffe3cc30904509e21bf15b78c45de1bac42a
Alec Robertson <alec.robertson08@gmail.com>
2023-03-20 17:28:18 -0400

    feat (nvim): add auto close tags treesitter plugin
    Also added KDL parser & playground plugin for debugging.

--- home/dot_config/nvim/lua/plugins/treesitter.lua
+++ home/dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,34 +1,54 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "bash",
-      "c",
-      "clojure",
-      "css",
-      "help",
-      "html",
-      "javascript",
-      "jsdoc",
-      "json",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "php",
-      "prisma",
-      "python",
-      "regex",
-      "rust",
-      "scss",
-      "sql",
-      "toml",
-      "tsx",
-      "typescript",
-      "vim",
-      "yaml",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      { "windwp/nvim-ts-autotag" },
     },
+    opts = {
+      autotag = {
+        enable = true,
+      },
+      ensure_installed = {
+        "bash",
+        "c",
+        "clojure",
+        "css",
+        "help",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "kdl",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "norg",
+        "php",
+        "prisma",
+        "python",
+        "query",
+        "regex",
+        "rust",
+        "scss",
+        "sql",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+    -- config = function()
+    --   -- TODO: figure out if this is doing anything
+    --   -- require("nvim-treesitter.install").compilers = { "gcc-12" }
+    -- end,
   },
 
   -- add jsonls and schemastore and set up treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },
+
+  -- add autotag plugin for auto closing and renaming HTML/TSX tags
+  -- { "windwp/nvim-ts-autotag" },
+
+  { "nvim-treesitter/playground" },
 }
