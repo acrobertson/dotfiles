0d91352d1bd690f80f3cfcd5dbd2274d0a01e5e3
Alec Robertson <alec.robertson08@gmail.com>
2023-03-20 17:23:33 -0400

    chore (nvim): remove colorscheme transparency

--- home/dot_config/nvim/lua/plugins/colorscheme.lua
+++ home/dot_config/nvim/lua/plugins/colorscheme.lua
@@ -3,11 +3,6 @@ return {
     "folke/tokyonight.nvim",
     opts = {
       style = "night",
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-      transparent = true,
     },
   },
 }
--- home/dot_config/nvim/lua/plugins/ui.lua
+++ home/dot_config/nvim/lua/plugins/ui.lua
@@ -1,12 +1,4 @@
 return {
-  -- set bg color for transparent colorscheme
-  {
-    "rcarriga/nvim-notify",
-    opts = {
-      background_colour = "#1a1b26",
-    },
-  },
-
   -- disable indent plugins if nolist is set
   {
     "lukas-reineke/indent-blankline.nvim",
