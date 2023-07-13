34b85fb58cd2c74ca656e76054bb96dc9444d1b1
Alec Robertson <alec.robertson08@gmail.com>
2023-07-13 11:27:02 -0400

    fix(nvim): add missing neogit setup
    Update was failing without it all of a sudden.

--- home/dot_config/nvim/lua/plugins/editor.lua
+++ home/dot_config/nvim/lua/plugins/editor.lua
@@ -1,6 +1,13 @@
 return {
   -- Git views
-  { "NeogitOrg/neogit", dependencies = "nvim-lua/plenary.nvim" },
+  {
+    "NeogitOrg/neogit",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = function()
+      require("neogit").setup({})
+    end,
+  },
+
   {
     "sindrets/diffview.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
