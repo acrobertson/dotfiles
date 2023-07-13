3655370378bccec97f0c7922e92b300abb0e60ac
Alec Robertson <alec.robertson08@gmail.com>
2023-07-13 13:22:26 -0400

    feat(nvim): customize neogit options
    Enabled telescope and diffview integrations. Also disabled the commit
    confirmation because of a conflict with `noice` that hasn't been fixed
    yet.

--- home/dot_config/nvim/lua/plugins/editor.lua
+++ home/dot_config/nvim/lua/plugins/editor.lua
@@ -1,15 +1,29 @@
 return {
   -- Git views
   {
-    "NeogitOrg/neogit",
-    dependencies = "nvim-lua/plenary.nvim",
-    config = function()
-      require("neogit").setup({})
-    end,
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
   },
 
   {
-    "sindrets/diffview.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    "NeogitOrg/neogit",
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+      { "sindrets/diffview.nvim" },
+    },
+    opts = {
+      disable_commit_confirmation = true,
+      use_telescope = true,
+      telescope_sorter = function()
+        return require("telescope").extensions.fzf.native_fzf_sorter()
+      end,
+      integrations = {
+        diffview = true,
+      },
+    },
+    config = function(_, opts)
+      require("neogit").setup(opts)
+    end,
   },
 }
