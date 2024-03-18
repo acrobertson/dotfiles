8311cfa0b76c3412a25f54d07490f49ce33129eb
Alec Robertson <alec.robertson08@gmail.com>
2024-03-17 23:52:55 -0400

    feat(nvim): add dark notify colorscheme toggle
    Auto switch color schemes when dark mode is toggled.

--- home/dot_config/nvim/lua/plugins/colorscheme.lua
+++ home/dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "cormacrelf/dark-notify",
+    config = function()
+      local dn = require("dark_notify")
+      dn.run()
+    end,
+  },
+
+  {
     "folke/tokyonight.nvim",
     opts = {
       style = "night",
