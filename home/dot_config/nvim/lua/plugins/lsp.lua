cb055748027b19199f3ca32f195bea4c70f117c4
Alec Robertson <alec.robertson08@gmail.com>
2024-06-09 23:30:39 -0400

    fix(nvim): prevent deno lsp conflicts
    Only attach deno lsp if there's a deno config file.

--- home/dot_config/nvim/lua/plugins/lsp.lua
+++ home/dot_config/nvim/lua/plugins/lsp.lua
@@ -29,6 +29,9 @@ return {
     opts = {
       ---@type lspconfig.options
       servers = {
+        denols = {
+          root_dir = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc"),
+        },
         cssls = {
           settings = {
             css = {
